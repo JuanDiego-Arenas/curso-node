a5241708aced938448ad23b219e2cc259c544a30
Juan Diego Win11 <arenas250421@gmail.com>
2024-08-20 16:10:54 -0500

    update package.json

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,11 +1,6 @@
-// config.js
+import { config } from 'dotenv';
+config();
 
 export const PORT = process.env.PORT || 3000;
-
-export const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/mydatabase';
-
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-
-export const SECRET_KEY = process.env.SECRET_KEY || 'mysecretkey';
-
-// Otros parámetros de configuración específicos de tu aplicación
+export const DB_URI = process.env.DB_URI;
+export const SECRET_KEY = process.env.SECRET_KEY;
